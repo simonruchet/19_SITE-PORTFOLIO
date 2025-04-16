a2f9f9e387f5b2595df64a205105677d46ef7287
simonruchet <simon.ruchet@studentfr.ch>
2025-04-16 12:02:35 +0200

    puce qui indique la page ou on se trouve dans le header

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-var projects = document.querySelector(".header-projects");
-var svg = document.querySelector(".header-svg");
-function openSvg() {
-  svg.classList.toggle("is-open");
-  projects.classList.toggle("is-active");
-}
-
-projects.addEventListener("click", openSvg);
